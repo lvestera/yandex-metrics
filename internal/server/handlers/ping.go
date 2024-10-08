5ea2c817e82975a92c9fd5037f65baa7fedfac33
smoiseenko <svetlana.moiseenko@altenar.com>
2024-10-08 17:35:57 +0300

    Fix DB in handler

--- internal/server/handlers/ping.go
+++ internal/server/handlers/ping.go
@@ -8,14 +8,14 @@ import (
 )
 
 type PingHandler struct {
-	Db *sql.DB
+	DB *sql.DB
 }
 
 func (ph PingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
-	if err := ph.Db.PingContext(ctx); err != nil {
+	if err := ph.DB.PingContext(ctx); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 
 	} else {
